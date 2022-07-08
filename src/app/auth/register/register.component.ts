543d282add0f7c4e3b4488b98169d6304e43603a
SergeyGSA <goreslavskiy.sergey@gmail.com>
2022-07-08 22:44:04 +0300

    added validators to avatar url in register form

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -20,9 +20,11 @@ export class RegisterComponent implements OnInit {
   public loaded$: Observable<boolean> = this.store$.pipe(select(getLoaded))
   public serverError$: Observable<string> = this.store$.pipe(select(getServerError))
 
+  private regexpUrl = /[-a-zA-Z0-9@:%_\+.~#?&\/=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&\/=]*)?/gi
+
   public get emailErrors(): string {
     if (this.registerForm.controls['email'].hasError('required')) {
-      return "Email can't be empty";
+      return "Email can't be empty"
     }
 
     return this.registerForm.controls['email'].hasError('email') ? 'Not a valid email' : ''
@@ -30,7 +32,7 @@ export class RegisterComponent implements OnInit {
 
   public get fullNameErrors(): string {
     if (this.registerForm.controls['fullName'].hasError('required')) {
-      return "Full name can't be empty";
+      return "Full name can't be empty"
     }
 
     return this.registerForm.controls['fullName'].hasError('minlength') ? 'Full name must be more than 3 symbols' : ''
@@ -38,12 +40,19 @@ export class RegisterComponent implements OnInit {
 
   public get passwordErrors(): string {
     if (this.registerForm.controls['password'].hasError('required')) {
-      return "Password can't be empty";
+      return "Password can't be empty"
     }
 
     return this.registerForm.controls['password'].hasError('minlength') ? 'Password must be more than 5 symbols' : ''
   }
-  //TODO: create validation getter for avatarUrl 
+ 
+  public get avatarUrlErrors(): string {
+    if (this.registerForm.controls['avatarUrl'].hasError('required')) {
+      return "Avatar url can't be empty" 
+    }
+
+    return this.registerForm.controls['avatarUrl'].hasError('pattern') ? 'Provide a valid url address' : '' 
+  }
 
   constructor( private store$: Store ) {}
 
@@ -67,8 +76,7 @@ export class RegisterComponent implements OnInit {
       email: new FormControl('', [Validators.required, Validators.email]),
       fullName: new FormControl('', [Validators.required, Validators.minLength(3)]),
       password: new FormControl('', [Validators.required, Validators.minLength(5)]),
-      // TODO: Add url regex for avatarUrl validators 
-      avatarUrl: new FormControl('', [Validators.required])
+      avatarUrl: new FormControl('', [Validators.required, Validators.pattern(this.regexpUrl)])
     }) 
   }
 }
